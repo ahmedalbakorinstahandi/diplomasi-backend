6b0a06fc03b267bd9713788d2953fa9978221909
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-12-16 15:05:28 +0300

    Update user_courses migration to use unsignedBigInteger for user_id, course_id, and subscription_id fields for improved data integrity

--- database/migrations/2025_12_03_105121_create_user_courses_table.php
+++ database/migrations/2025_12_03_105121_create_user_courses_table.php
@@ -15,11 +15,11 @@ return new class extends Migration
 
         Schema::create('user_courses', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->bigInteger('course_id');
+            $table->unsignedBigInteger('course_id');
             $table->foreign('course_id')->references('id')->on('courses');
-            $table->bigInteger('subscription_id')->nullable();
+            $table->unsignedBigInteger('subscription_id')->nullable();
             $table->foreign('subscription_id')->references('id')->on('subscriptions');
             $table->enum('status', ["active","completed","expired","cancelled"]);
             $table->timestamp('started_at')->nullable();
