5125e9040a990a5e9ea8bf4d510f5489f4609e00
ahmed-albakor <ahmed.albakor206@gmail.com>
2025-12-20 11:43:47 +0300

    Update email validation in RegisterRequest to remove unique constraint for improved registration flexibility

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -14,7 +14,8 @@ class RegisterRequest extends BaseFormRequest
         return [
             'first_name' => 'required|string|max:50',
             'last_name' => 'required|string|max:50',
-            'email' => ['required', 'email', Rule::unique('users', 'email')->whereNull('deleted_at')],
+            'email' => ['required', 'email'],
+            // 'email' => ['required', 'email', Rule::unique('users', 'email')->whereNull('deleted_at')],
             'password' => 'required|string|min:6|max:255|confirmed',
             'avatar' => 'nullable|string|max:100',
             'phone' => 'required|string|max:50',
